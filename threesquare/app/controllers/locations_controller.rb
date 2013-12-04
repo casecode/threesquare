30d55548216a173f9876d01039723472d272a1e1
casecode <casey.r.white@gmail.com>
2013-12-04 14:32:01 -0600

    Create locations controller skeleton

---
+++ threesquare/app/controllers/locations_controller.rb
@@ -0,0 +1,27 @@
+class LocationsController < ApplicationController
+	
+	def landing_page
+	end
+	
+	def show
+	end
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
