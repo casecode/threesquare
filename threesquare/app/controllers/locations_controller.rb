995f6d70a9629cbaa5ac5aa4fdcc2f349486207d
casecode <casey.r.white@gmail.com>
2013-12-04 20:08:00 -0600

    Add new view and action, as well as create action and private location_params method

--- threesquare/app/controllers/locations_controller.rb
+++ threesquare/app/controllers/locations_controller.rb
@@ -12,9 +12,16 @@ class LocationsController < ApplicationController
 	end
 
 	def new
+		@location = Location.new
 	end
 
 	def create
+		@location = Location.new(location_params)
+		@location.save
+
+		# After creating a new location review,
+		# redirect to see newly created review
+		redirect_to location_path(@location)
 	end
 
 	def edit
@@ -24,6 +31,11 @@ class LocationsController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+	def location_params
+		params.require(:location).permit(:name, :picture_url, :review)
 	end
 
 end
